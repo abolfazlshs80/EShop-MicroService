ccfb4fe972eb009bc1418650a67193dfe96003aa
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 21:37:37 +0330

    ✨ feat(user): add IUserCommandRepository interface
    - create IUserCommandRepository interface for command operations
    - inherit from IBaseCommonRepository for common repository functionalities

---
+++ Src/Services/IDP.Service/IDP.Domain/IRepository/Command/IUserCommandrepository.cs
@@ -0,0 +1,14 @@
+﻿using IDP.Domain.Entities;
+using IDP.Domain.IRepository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Domain.IRepository.Command
+{
+    public interface IUserCommandrepository:IBaseCommonRepository<User>
+    {
+    }
+}
