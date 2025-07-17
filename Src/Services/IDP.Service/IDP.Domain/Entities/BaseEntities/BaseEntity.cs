643a8ad879ec6107ea9da1fc249a3cec104e057b
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 21:37:26 +0330

    ✨ feat(entity): add base entity for common properties
    - introduce BaseEntity class with Id, CreateDate, and UpdateDate
    - set default CreateDate to DateTime.UtcNow in constructor

---
+++ Src/Services/IDP.Service/IDP.Domain/Entities/BaseEntities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Domain.Entities.BaseEntities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreateDate = DateTime.UtcNow;
+        }
+        public Int64 Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
