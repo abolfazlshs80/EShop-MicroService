57cdf15759b0de7702816af6334e7b60e92249d1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 21:36:56 +0330

    ✨ feat(user): implement create user command handler
    - add handler for creating new users
    - implement insert user repository for data access

---
+++ Src/Services/IDP.Service/IDP.Application/Handler/Command/User/UserHandler.cs
@@ -0,0 +1,20 @@
+﻿using IDP.Application.Command.User;
+using IDP.Domain.IRepository;
+using IDP.Domain.IRepository.Command;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Application.Handler.Command.User
+{
+    public class UserHandler(IUserRepository userRepository) : IRequestHandler<CreateUser, bool>
+    {
+        public async Task<bool> Handle(CreateUser request, CancellationToken cancellationToken)
+        {
+            return await userRepository.Insert(new Domain.Entities.User { CodeNumber=request.CodeNumber,FullName=request.FullName});
+        }
+    }
+}
