a77ad917334ffdd394629d0293d3196d7107c84c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 21:36:51 +0330

    ✨ feat(user): implement create user command
    - add create user command with full name and code number
    - add validation for full name

---
+++ Src/Services/IDP.Service/IDP.Application/Command/User/CreateUser.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Application.Command.User
+{
+    public record CreateUser:IRequest<bool>
+    {
+        [Required]
+        [MinLength(3)]
+        [DefaultValue("System")]
+        public required string FullName { get; set; }
+        public required string CodeNumber { get; set; }
+    }
+}
