6996a6bc6a4579202974f1f1e1577c8bac509a1c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 21:36:40 +0330

    ✨ feat(user): implement user creation endpoint with versioning - add v1 and v2 endpoints for user creation - use mediatr to handle user creation command

--- Src/Services/IDP.Service/IDP.Api/Controllers/V1/UsersController.cs
+++ Src/Services/IDP.Service/IDP.Api/Controllers/V1/UsersController.cs
@@ -1,12 +1,29 @@
-﻿using IDP.Api.Controllers.Base;
+﻿using Asp.Versioning;
+using IDP.Api.Controllers.Base;
+using IDP.Application.Command.User;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IDP.Api.Controllers.V1
 {
-    [Route("api/v1/users")]
+    [ApiVersion(1)]
+    [ApiVersion(2)]
     [ApiController]
-    public class UsersController : IBaseController
+    [Route("api/v{v:apiVersion}/Users")]
+    public class UsersController(IMediator _mediator) : IBaseController
     {
+        [MapToApiVersion(1)]
+        [HttpPost]
+        public async Task<IActionResult> Insert1(CreateUser user)
+        {
+            return Ok(await _mediator.Send(user));
+        }
+        [MapToApiVersion(2)]
+        [HttpPost]
+        public async Task<IActionResult> Insert2(CreateUser user)
+        {
+            return Ok(await _mediator.Send(user));
+        }
     }
 }
