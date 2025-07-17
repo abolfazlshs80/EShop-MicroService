707aba916b8da8df41444bec196c4de860897b57
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 21:37:31 +0330

    ✨ feat(repository): add base common repository interface   - define basic CRUD operations for repositories   - create IBaseCommonRepository interface with Create, Update, Delete methods

---
+++ Src/Services/IDP.Service/IDP.Domain/IRepository/Base/IBaseCommonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Domain.IRepository.Base
+{
+    public interface IBaseCommonRepository<in T> where T : class
+    {
+        Task<bool>Create(T entity);
+        Task<bool>Update(T entity);
+        Task<bool>Delete(T entity);
+    }
+}
