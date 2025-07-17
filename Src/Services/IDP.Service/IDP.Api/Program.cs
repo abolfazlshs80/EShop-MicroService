ae06c84bdb763c2119a227d801ee8b90f4a62dfd
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 21:36:35 +0330

    ✨ feat(api): implement api versioning and mediatr
    - add api versioning using Asp.Versioning
      - configure default version, reporting, and reader
    - add mediatr for command handling
      - register UserHandler and UserRepository

--- Src/Services/IDP.Service/IDP.Api/Program.cs
+++ Src/Services/IDP.Service/IDP.Api/Program.cs
@@ -1,3 +1,10 @@
+using Asp.Versioning;
+using IDP.Application.Handler.Command.User;
+using IDP.Domain.IRepository.Command;
+using IDP.Infra.Repositories;
+using MediatR;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +13,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(typeof(UserHandler).GetTypeInfo().Assembly);
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));
+})
+.AddMvc() // This is needed for controllers
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
