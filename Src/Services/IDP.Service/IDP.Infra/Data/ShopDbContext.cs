c33e769b9ffd19e630369aba44d930e1d3f52fcd
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-18 18:52:39 +0330

    ✨ feat(data): add shopdbcontext for database interaction - introduce shopdbcontext for managing user entities - implement shopdbcontextfactory for design-time db context creation

---
+++ Src/Services/IDP.Service/IDP.Infra/Data/ShopDbContext.cs
@@ -0,0 +1,40 @@
+﻿using IDP.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace IDP.Infra.Data
+{
+    public class ShopDbContext:DbContext
+    {
+
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<User> User { get; set; }
+
+    }
+
+    public class ShopDbContextFactory : IDesignTimeDbContextFactory<ShopDbContext>
+    {
+        public ShopDbContext CreateDbContext(string[] args)
+        {
+            // بارگذاری appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            // ایجاد DbContextOptions
+            var optionsBuilder = new DbContextOptionsBuilder<ShopDbContext>();
+            var connectionString = configuration.GetConnectionString("CommandDBConnection");
+            optionsBuilder.UseSqlServer(connectionString); // یا UseNpgsql برای PostgreSQL
+
+            return new ShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
