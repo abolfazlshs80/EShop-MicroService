4351f7fb9942e6b82e61bd1c254d015b802d0025
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-18 18:52:24 +0330

    ✨ feat(auth): implement login authentication handler
    - add LoginAuthHandler to handle user login requests
    - create JWT token upon successful authentication

---
+++ Src/Services/IDP.Service/IDP.Application/Handler/Query/Auth/LoginAuthHandler.cs
@@ -0,0 +1,20 @@
+﻿using Auth;
+using IDP.Application.Query.Auth;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Application.Handler.Query.Auth
+{
+    public class LoginAuthHandler(IJwtHandler jwtHandler) : IRequestHandler<LoginUser,bool>
+    {
+        public async Task<bool> Handle(LoginUser request, CancellationToken cancellationToken)
+        {
+            var token = jwtHandler.Create(22);
+            return true;
+        }
+    }
+}
