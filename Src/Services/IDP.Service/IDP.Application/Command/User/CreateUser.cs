19513a86104323b82977e9531a52d4a5c9fbc77f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-18 18:52:14 +0330

    ✨ feat(user): add username, password, and salt properties to CreateUser command
    - Add new properties to the CreateUser command for handling user authentication data.
    - These properties include UserName, Password, and Salt, which are required for creating user accounts.

--- Src/Services/IDP.Service/IDP.Application/Command/User/CreateUser.cs
+++ Src/Services/IDP.Service/IDP.Application/Command/User/CreateUser.cs
@@ -16,5 +16,9 @@ namespace IDP.Application.Command.User
         [DefaultValue("System")]
         public required string FullName { get; set; }
         public required string CodeNumber { get; set; }
+
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public required string Salt { get; set; }
     }
 }
