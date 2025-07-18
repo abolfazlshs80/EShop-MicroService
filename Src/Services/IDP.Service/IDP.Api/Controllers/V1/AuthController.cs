b62d5f7d629ec305367e911073298207dac1368b
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-18 18:52:04 +0330

    ✨ feat(auth): implement user login endpoint
    - create AuthController with login functionality
    - add route for user authentication

---
+++ Src/Services/IDP.Service/IDP.Api/Controllers/V1/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Asp.Versioning;
+using IDP.Api.Controllers.Base;
+using IDP.Application.Command.User;
+using IDP.Application.Query.Auth;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Api.Controllers.V1
+{
+    [ApiVersion(1)]
+    [ApiVersion(2)]
+    [ApiController]
+    [Route("api/v{v:apiVersion}/Auth")]
+    public class AuthController(IMediator _mediator) : IBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginUser user)
+        {
+            return Ok(await _mediator.Send(user));
+        }
+      
+    }
+}
