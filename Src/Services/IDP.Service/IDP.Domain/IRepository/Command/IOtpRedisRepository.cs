fb0621ad159eb43d78a97f85ae194e42295c7795
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:57:37 +0330

    ✨ feat(otp): add IOtpRedisRepository for OTP data access
    - create IOtpRedisRepository interface for managing OTP data in Redis
    - inherit from IBaseCommonRepository for common repository functions
    - define Getdata method to retrieve OTP data by mobile number

---
+++ Src/Services/IDP.Service/IDP.Domain/IRepository/Command/IOtpRedisRepository.cs
@@ -0,0 +1,10 @@
+﻿using IDP.Domain.DTOs;
+using IDP.Domain.IRepository.Base;
+
+namespace IDP.Domain.IRepository.Command
+{
+    public interface IOtpRedisRepository : IBaseCommonRepository<Otp>
+    {
+        Task<Otp> Getdata(string mobile);
+    }
+}
