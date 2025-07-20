686dbdbae839a149ad151c4975f759bb3c23e2a3
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:59:12 +0330

    ✨ feat(user): create user query repository
    - implement IUserQueryRepository
    - add GetUserAsync method to retrieve user by mobile number

---
+++ Src/Services/IDP.Service/IDP.Infra/Repositories/Query/UserQueryRepository.cs
@@ -0,0 +1,20 @@
+﻿using IDP.Domain.Entities;
+using IDP.Domain.IRepository.Query;
+using IDP.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDP.Infra.Repositories.Query;
+
+public class UserQueryRepository : IUserQueryRepository
+{
+    private readonly ShopQueryDbContext _db;
+    public UserQueryRepository(ShopQueryDbContext shopQueryDbContext)
+    {
+            _db = shopQueryDbContext;
+    }
+    public async Task<User> GetUserAsync(string mobilenumber)
+    {
+        var userfound=await _db.Tbl_Users.FirstOrDefaultAsync(p=>p.MobileNumber==mobilenumber);
+        return userfound;
+    }
+}
