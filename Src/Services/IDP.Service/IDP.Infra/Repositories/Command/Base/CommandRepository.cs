9ef5c951569a150bbffe86121d3ea6b89fe2c7b2
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:59:06 +0330

    ✨ feat(repository): implement base command repository
    - create generic command repository for basic CRUD operations
    - add insert, update, and delete functionalities

---
+++ Src/Services/IDP.Service/IDP.Infra/Repositories/Command/Base/CommandRepository.cs
@@ -0,0 +1,47 @@
+﻿using IDP.Domain.IRepository.Base;
+using IDP.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDP.Infra.Repository.Command.Base;
+
+public class CommandRepository<T> : IBaseCommonRepository<T> where T : class
+{
+    protected readonly ShopCommandDbContext _context;
+
+    public CommandRepository(ShopCommandDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<T> Insert(T entity)
+    {
+
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<bool> Update(T entity)
+    {
+        try
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return true;    
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+    }
+
+    public async Task<bool> Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+
+}
