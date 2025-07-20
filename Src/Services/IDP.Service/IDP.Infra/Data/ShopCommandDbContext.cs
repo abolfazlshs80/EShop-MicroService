24642e9a0fc0f8f817ee89bdb3bc446233a652bf
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:58:22 +0330

    ✨ feat(data): add shop command dbcontext
    - create shop command dbcontext for data access
    - configure connection to sql server using connection string

---
+++ Src/Services/IDP.Service/IDP.Infra/Data/ShopCommandDbContext.cs
@@ -0,0 +1,31 @@
+﻿using IDP.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace IDP.Infra.Data
+{
+    public class ShopCommandDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public ShopCommandDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("CommandDBConnection"));
+        }
+        public DbSet<User> Tbl_Users { get; set; }
+        //     public DbSet<Outbox> Tbl_Outbox { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
