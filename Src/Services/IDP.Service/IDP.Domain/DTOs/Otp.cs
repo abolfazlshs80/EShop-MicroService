08b09f4d09b885b1d5ee19ce9b7b3e810a3416e9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:58:06 +0330

    ✨ feat(otp): implement otp dto
    - create otp data transfer object
    - add username, otpcode and isuse properties

---
+++ Src/Services/IDP.Service/IDP.Domain/DTOs/Otp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Domain.DTOs
+{
+    public class Otp
+    {
+        public required string   UserName{ get; set; }
+
+        public required int OtpCode { get; set; }
+        public bool IsUse { get; set; }
+    }
+}
