6ae56b4f51044b2eab62dc2d1f3710ed5ca57d68
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:56:49 +0330

    ✨ feat(auth): implement register and send otp endpoint
    - add register and send otp endpoint to auth controller
    - implement login mobile user command

--- Src/Services/IDP.Service/IDP.Api/Controllers/V1/AuthController.cs
+++ Src/Services/IDP.Service/IDP.Api/Controllers/V1/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.Versioning;
 using IDP.Api.Controllers.Base;
+using IDP.Application.Command.Auth;
 using IDP.Application.Command.User;
 using IDP.Application.Query.Auth;
 using MediatR;
@@ -19,6 +20,22 @@ namespace IDP.Api.Controllers.V1
         {
             return Ok(await _mediator.Send(user));
         }
-      
+
+
+        /// <summary>
+        ///    ثبت نام وارسال كد به شماره موبايل
+        /// </summary>
+        /// <param name="authQuery"></param>
+        /// <returns></returns>
+        [HttpPost("RegisterAndSendOtp")]
+
+        public async Task<IActionResult> RegisterAndSendOtp([FromBody] LoginMobileUser authQuery)
+        {
+
+            var res = await _mediator.Send(authQuery);
+
+            return Ok(res);
+        }
+
     }
 }
