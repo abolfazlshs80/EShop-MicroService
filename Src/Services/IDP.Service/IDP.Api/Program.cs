deaae64a509d6bd3cdabcc135d19f7cd2886f535
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:56:43 +0330

    ✨ feat(IDP): integrate MediatR and CQRS pattern
    - add MediatR for handling commands and queries
    - implement CQRS pattern for separation of concerns
    - register handlers for user and auth commands/queries
    - add repositories for command and query operations
    - configure database context for command and query sides
    - integrate Redis cache for OTP storage
    - set up RabbitMQ for message queue
    - configure CAP for distributed transactions
    - add AutoMapper for object mapping

--- Src/Services/IDP.Service/IDP.Api/Program.cs
+++ Src/Services/IDP.Service/IDP.Api/Program.cs
@@ -1,24 +1,79 @@
 using Asp.Versioning;
 using Auth;
+using AutoMapper;
+using IDP.Application.Handler.Command.Auth;
 using IDP.Application.Handler.Command.User;
 using IDP.Application.Handler.Query.Auth;
+using IDP.Application.Helper;
+using IDP.Domain.IRepository.Base;
 using IDP.Domain.IRepository.Command;
+using IDP.Domain.IRepository.Query;
 using IDP.Infra.Data;
+using IDP.Infra.Repositories.Command;
+using IDP.Infra.Repositories.Query;
+using IDP.Infra.Repository.Command;
+using IDP.Infra.Repository.Command.Base;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MappingProfiles());
+});
 
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(opition =>
+{
+    opition.Configuration = builder.Configuration.GetValue<string>("CashSetting:RedisUrl");
+    opition.InstanceName = "SampleInstance";
+});
 builder.Services.AddMediatR(typeof(UserHandler).GetTypeInfo().Assembly);
 builder.Services.AddMediatR(typeof(LoginAuthHandler).GetTypeInfo().Assembly);
+builder.Services.AddMediatR(typeof(AuthLoginCommandHandler).GetTypeInfo().Assembly);
 //builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IOtpRedisRepository, OtpRedisRepository>();
+builder.Services.AddScoped<IUserCommandRepository, UserCommandRepository>();
+builder.Services.AddScoped<IUserQueryRepository, UserQueryRepository>();
+builder.Services.AddScoped(typeof(IBaseCommonRepository<>), typeof(CommandRepository<>));
+builder.Services.AddScoped<IOtpRedisRepository, OtpRedisRepository>();
+
+#region messageMq   
+
+builder.Services.AddCap(options =>
+{
+    options.UseEntityFramework<ShopCommandDbContext>();
+    options.UseDashboard(path => path.PathMatch = "/cap");
+    options.UseRabbitMQ(options =>
+    {
+        options.ConnectionFactoryOptions = options =>
+        {
+            options.Ssl.Enabled = false;
+            options.HostName = builder.Configuration["Cap:HostName"];
+            options.UserName = builder.Configuration["Cap:UserName"];
+            options.Password = builder.Configuration["Cap:Password"];
+            options.Port = int.Parse( builder.Configuration["Cap:Port"]);
+        };
+
+    });
+    options.FailedRetryCount = int.Parse( builder.Configuration["Cap:FailedRetryCount"]);
+    options.FailedRetryInterval = int.Parse( builder.Configuration["Cap:FailedRetryInterval"]);
+
+
+});
+
+#endregion
+
 
 Extensions.AddJwt(builder.Services, builder.Configuration);
 builder.Services.AddApiVersioning(options =>
@@ -38,8 +93,8 @@ builder.Services.AddApiVersioning(options =>
     options.SubstituteApiVersionInUrl = true;
 });
 
-builder.Services.AddDbContext<ShopDbContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("CommandDBConnection")));
+builder.Services.AddTransient<ShopCommandDbContext>();
+builder.Services.AddTransient<ShopQueryDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
