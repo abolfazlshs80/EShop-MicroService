f2d74ec4b8a7beb6898ce54ad1c66a0a56191147
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:57:03 +0330

    ✨ feat(auth): implement login authentication with OTP verification
    - integrate redis to store and validate OTP codes
    - generate JWT token upon successful OTP verification
    - add exception handling for robustness

--- Src/Services/IDP.Service/IDP.Application/Handler/Query/Auth/LoginAuthHandler.cs
+++ Src/Services/IDP.Service/IDP.Application/Handler/Query/Auth/LoginAuthHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Auth;
 using IDP.Application.Query.Auth;
+using IDP.Domain.IRepository.Command;
+using IDP.Domain.IRepository.Query;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -9,12 +11,41 @@ using System.Threading.Tasks;
 
 namespace IDP.Application.Handler.Query.Auth
 {
-    public class LoginAuthHandler(IJwtHandler jwtHandler) : IRequestHandler<LoginUser,bool>
+    public class LoginAuthHandler : IRequestHandler<LoginUser, JsonWebToken>
     {
-        public async Task<bool> Handle(LoginUser request, CancellationToken cancellationToken)
+        private readonly IJwtHandler _jwtHandler;
+        private readonly IOtpRedisRepository _redisRepository;
+        private readonly IUserQueryRepository _userQueryRepository;
+        public LoginAuthHandler(IJwtHandler jwtHandler, IOtpRedisRepository otpRedisRepository, IUserQueryRepository userQueryRepository)
         {
-            var token = jwtHandler.Create(22);
-            return true;
+            _jwtHandler = jwtHandler;
+            _redisRepository = otpRedisRepository;
+            _userQueryRepository = userQueryRepository;
+        }
+        public async Task<JsonWebToken> Handle(LoginUser request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var res = await _redisRepository.Getdata(request.PhoneNumber);
+                if (res == null) return null;
+                if (res.OtpCode == request.Code)
+                {
+                    var user = await _userQueryRepository.GetUserAsync(request.PhoneNumber);
+                    var token = _jwtHandler.Create(user.Id);
+                    return token;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
         }
     }
 }
