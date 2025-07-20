63e20292d3a5a10f16f645bd4d84c782023c671f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:57:14 +0330

    ✨ feat(auth): implement user login with OTP verification
    - add AuthLoginCommandHandler to handle user login via mobile number
    - generate and store OTP in Redis if user exists or create new user
    - send OTP via notification service (commented out)
    diff --git a/Src/Services/IDP.Service/IDP.Application/Command/Auth/AuthLoginCommand.cs b/Src/Services/IDP.Service/IDP.Application/Command/Auth/AuthLoginCommand.cs
    new file mode 100644
    index 0000000..6674097
    --- /dev/null
    +++ b/Src/Services/IDP.Service/IDP.Application/Command/Auth/AuthLoginCommand.cs
    @@ -0,0 +1,13 @@
    -﻿using MediatR;
    +﻿using IDP.Domain.DTOs;
    +using MediatR;
    +
    +namespace IDP.Application.Command.Auth
    +{
    +    public class LoginMobileUser : IRequest<bool>
    +    {
    +        public string MobileNumber { get; set; }
    +        public string Password { get; set; }
    +
    +    }
    +}
    +
    \ No newline at end of file

---
+++ Src/Services/IDP.Service/IDP.Application/Handler/Command/Auth/AuthLoginCommand.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using DotNetCore.CAP;
+using IDP.Application.Command.Auth;
+using IDP.Domain.DTOs;
+using IDP.Domain.Entities;
+using IDP.Domain.IRepository.Command;
+using IDP.Domain.IRepository.Query;
+using MediatR;
+
+namespace IDP.Application.Handler.Command.Auth
+{
+    public class AuthLoginCommandHandler : IRequestHandler<LoginMobileUser, bool>
+    {
+        private readonly IOtpRedisRepository _otpRedisRepository;
+        private readonly IUserCommandRepository _userCommandRepository;
+        private readonly IUserQueryRepository _userQueryRepository;
+        private readonly IMapper _mapper;
+        private readonly ICapPublisher _capBus;
+
+        public AuthLoginCommandHandler(IOtpRedisRepository otpRedisRepository,
+            IUserCommandRepository userCommandRepository,
+           ICapPublisher capPublisher,
+            IUserQueryRepository userQueryRepository, IMapper mapper)
+        {
+            _otpRedisRepository = otpRedisRepository;
+            _userCommandRepository = userCommandRepository;
+            _userQueryRepository = userQueryRepository;
+            _mapper = mapper;
+
+            _capBus = capPublisher;
+        }
+        public async Task<bool> Handle(LoginMobileUser request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var userobj = _mapper.Map<IDP.Domain.Entities.User>(request);
+                var user = await _userQueryRepository.GetUserAsync(request.MobileNumber);
+                if (user == null)
+                {
+                    Random random = new Random();
+                    var code = random.Next(1000, 10000);
+                    //ارسال پیامک به سرویس نوتفیکیشن
+                    //await _capBus.PublishAsync<AuthCommand>("otpevent", new AuthCommand
+                    //{
+                    //    MobileNumber = request.MobileNumber,
+                    //});
+                    //await _publishEndpoint.Publish<OtpEvents>(new OtpEvents
+                    //{
+
+                    //    MobileNumber = request.MobileNumber,
+                    //    OtpCode = code.ToString(),
+                    //});
+
+                    userobj.UserName = request.MobileNumber;
+                    var res = await _userCommandRepository.Insert(userobj);
+                    await _otpRedisRepository.Insert(new Otp { UserName = userobj.MobileNumber, OtpCode = code, IsUse = false });
+                }
+                else
+                {
+                    Random random = new Random();
+                    var code = random.Next(1000, 10000);
+                    //ارسال پیامک به سزویس نوتفیکیشن
+                    //await _capBus.PublishAsync<AuthCommand>("otpevent", new AuthCommand
+                    //{
+                    //    MobileNumber = request.MobileNumber,
+                    //});
+                    //await _publishEndpoint.Publish<OtpEvents>(new OtpEvents
+                    //{
+
+                    //    MobileNumber = request.MobileNumber,
+                    //    OtpCode = code.ToString(),
+                    //});
+                    userobj.UserName = request.MobileNumber;
+                    await _otpRedisRepository.Insert(new Otp { UserName = user.MobileNumber, OtpCode = code, IsUse = false });
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
