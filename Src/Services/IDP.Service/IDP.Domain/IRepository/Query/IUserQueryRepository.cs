283cab6946e2f89d57dc79b7150e657275742320
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:57:41 +0330

    ✨ feat(user): create IUserQueryRepository
    - add interface for user query repository

---
+++ Src/Services/IDP.Service/IDP.Domain/IRepository/Query/IUserQueryRepository.cs
@@ -0,0 +1,9 @@
+﻿using IDP.Domain.Entities;
+
+namespace IDP.Domain.IRepository.Query
+{
+    public interface IUserQueryRepository
+    {
+        Task<User> GetUserAsync(string mobilenumber);    
+    }
+}
