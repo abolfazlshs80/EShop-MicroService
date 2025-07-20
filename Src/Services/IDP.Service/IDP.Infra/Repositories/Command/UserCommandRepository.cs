06d4c9fe0869a85b2081fac87e05754b239a9546
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 11:59:00 +0330

    ✨ feat(user): add user command repository
    - implement user command repository for handling user data operations

---
+++ Src/Services/IDP.Service/IDP.Infra/Repositories/Command/UserCommandRepository.cs
@@ -0,0 +1,19 @@
+﻿using IDP.Domain.Entities;
+using IDP.Domain.IRepository.Command;
+using IDP.Infra.Data;
+using IDP.Infra.Repository.Command.Base;
+
+namespace IDP.Infra.Repository.Command
+{
+    public class UserCommandRepository : CommandRepository<User>, IUserCommandRepository
+    {
+        private readonly ShopCommandDbContext shopCommandDbContext;
+       
+        public UserCommandRepository(ShopCommandDbContext context) : base(context)
+        {
+            shopCommandDbContext = context; 
+           
+        }
+      
+    }
+}
